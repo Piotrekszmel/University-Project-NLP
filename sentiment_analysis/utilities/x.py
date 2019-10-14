a135d15ee80e4f150aabd355f4bdb3098247c8cf
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-14 18:28:53 +0200

    -mcreate get_labels_to_categories_map

--- sentiment_analysis/utilities/x.py
+++ sentiment_analysis/utilities/x.py
@@ -1,7 +1,3 @@
-from collections import Counter
-from keras.utils import np_utils
-from sklearn.preprocessing import LabelEncoder
-from sklearn.utils import compute_class_weight
 import numpy as np
 
 
@@ -14,19 +10,9 @@ def get_class_labels(y):
   return np.unique(y)
 
 
-def labels_to_categories(y):
-  """
-    Labels to categories
-    :param y: list of labels, ex. ['positive', 'negative', 'positive', 'neutral', 'positive', ...]
-    :return: list of categories, ex. [0, 2, 1, 2, 0, ...]
-  """
-  encoder = LabelEncoder()
-  encoder.fit(y)
-  y_num = encoder.transform(y)
-  return y_num
 
 
-def get_labels_to_categories_map(y):
+def labels_to_categories_map(y):
   """
     Get the mapping of class labels to numerical categories
     :param y: list of labels, ex. ['positive', 'negative', 'positive', 'neutral', 'positive', ...]
@@ -34,6 +20,6 @@ def get_labels_to_categories_map(y):
   """
   labels = get_class_labels(y)
   return {l: i for i, l in enumerate(labels)}
-  
 
 
+print(labels_to_categories_map([1,3,7, 11, 11, 11, 7, 3, 1, 1]))
