1e80e1bf743f785400a6750dbc535a72805a8c51
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-15 18:04:16 +0200

    create class_weights2 that normalize weights base on frequencies

--- sentiment_analysis/utilities/data_preparation.py
+++ sentiment_analysis/utilities/data_preparation.py
@@ -65,5 +65,26 @@ def get_class_weights(y):
 
   d = {c: w for c, w in zip(np.unique(y), weights)}
 
-  
+  return d
+
+
+def class_weights2(y, smooth_factor=0):
+  """
+    Returns the normalized weights for each class based on the frequencies of the samples
+    :param smooth_factor: factor that smooths extremely uneven weights
+    :param y: list of true labels (the labels must be hashable)
+    :return: dictionary with the weight for each class
+  """
+  counter = Counter(y)
+
+  if smooth_factor > 0:
+    p = max(counter.values()) * smooth_factor
+    for k in counter.keys():
+      counter[k] += p
+
+  majority = max(counter.values())
+  print("\n\n")
+  for cls, count in counter.items():
+    print(cls, " -> " , count)
+  return {cls: float(majority / count) for cls, count in counter.items()}
 
