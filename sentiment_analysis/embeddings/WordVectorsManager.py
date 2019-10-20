f3b9dbc758cd4d703a18504209d14d94885419c6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-20 22:59:39 +0200

    create WordVectorsManager and add init, is_ascii and write functions

--- sentiment_analysis/embeddings/WordVectorsManager.py
+++ sentiment_analysis/embeddings/WordVectorsManager.py
@@ -8,6 +8,18 @@ from utilities.ResourceManager import ResourceManager
 
 
 class WordVectorsManager(ResourceManager):
-  pass
+    def __init__(self, corpus=None, dim=None, omit_non_english=False):
+        super().__init__()
 
-print('DONE')
+        self.omit_non_english = omit_non_english
+        self.wv_filename = "{}.{}d.txt".format(corpus, str(dim))
+        self.parsed_filename = "{}.{}d.pickle".format(corpus, str(dim))
+
+    def is_ascii(self, text):
+      try:
+        text.encode('ascii')
+        return True
+      except:
+        return False
+
+    def write(self):
