a56763af28c7232182d8920ff9f003c67a62c168
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-25 00:21:41 +0200

    -mcreate call method for Attention layer

--- sentiment_analysis/utilities/layers.py
+++ sentiment_analysis/utilities/layers.py
@@ -35,34 +35,34 @@ class MeanOverTime(Layer):
 
 
 class Attention(Layer):
-  def __init__(self,
+    def __init__(self,
                  W_regularizer=None, b_regularizer=None,
                  W_constraint=None, b_constraint=None,
                  bias=True,
                  return_attention=False,
                  **kwargs):
 
-    """
-    Keras Layer that implements an Attention mechanism for temporal data.
-    Supports Masking.
-    Follows the work of Raffel et al. [https://arxiv.org/abs/1512.08756]
-    # Input shape
-      3D tensor with shape: `(samples, steps, features)`.
-    # Output shape
-       2D tensor with shape: `(samples, features)`.
-    :param kwargs:
-      Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
-      The dimensions are inferred based on the output shape of the RNN.
-      Note: The layer has been tested with Keras 1.x
-      Example:
-          # 1
-          model.add(LSTM(64, return_sequences=True))
-          model.add(Attention())
-          # next add a Dense layer (for classification/regression) or whatever...
-          # 2 - Get the attention scores
-          hidden = LSTM(64, return_sequences=True)(words)
-          sentence, word_scores = Attention(return_attention=True)(hidden)
-    """
+        """
+        Keras Layer that implements an Attention mechanism for temporal data.
+        Supports Masking.
+        Follows the work of Raffel et al. [https://arxiv.org/abs/1512.08756]
+        # Input shape
+        3D tensor with shape: `(samples, steps, features)`.
+        # Output shape
+        2D tensor with shape: `(samples, features)`.
+        :param kwargs:
+        Just put it on top of an RNN Layer (GRU/LSTM/SimpleRNN) with return_sequences=True.
+        The dimensions are inferred based on the output shape of the RNN.
+        Note: The layer has been tested with Keras 1.x
+        Example:
+            # 1
+            model.add(LSTM(64, return_sequences=True))
+            model.add(Attention())
+            # next add a Dense layer (for classification/regression) or whatever...
+            # 2 - Get the attention scores
+            hidden = LSTM(64, return_sequences=True)(words)
+            sentence, word_scores = Attention(return_attention=True)(hidden)
+        """
     
     self.supports_masking = True
     self.return_attention = return_attention
@@ -77,22 +77,51 @@ class Attention(Layer):
     self.bias = bias
     super().__init__(**kwargs)
 
-  def build(self, input_shape):
-    assert len(input_shape) == 3
+    def build(self, input_shape):
+        assert len(input_shape) == 3
 
-    self.W = self.add_weight(name='{}_W'.format(self.name), 
-                                 shape=(input_shape[-1],),
-                                 initializer=self.init,
-                                 regularizer=self.W_regularizer,
-                                 constraint=self.W_constraint)
-    if self.bias:
-      self.b = self.add_weight(name='{}_b'.format(self.name),
-                               shape=(input_shape[1],), 
-                               initializer="zero",
-                               regularizer=self.b_regularizer,
-                               constraint=self.b_constraint)
-    else:
-      self.b = None
+        self.W = self.add_weight(name='{}_W'.format(self.name), 
+                                    shape=(input_shape[-1],),
+                                    initializer=self.init,
+                                    regularizer=self.W_regularizer,
+                                    constraint=self.W_constraint)
+        if self.bias:
+            self.b = self.add_weight(name='{}_b'.format(self.name),
+                                    shape=(input_shape[1],), 
+                                    initializer="zero",
+                                    regularizer=self.b_regularizer,
+                                    constraint=self.b_constraint)
+        else:
+            self.b = None
+        
+        self.built = True
     
-    self.built = True
+    def compute_mask(self, input, input_mask=None):
+        # do not pass the mask to the next layers
+        return None
+    
+    def call(self, x, mask=None):
+        eij = dot_product(x, self.W)
+
+        if self.bias:
+            eij += self.b
+        
+        eij = K.tanh(eij)
+
+        a = K.exp(eij)
+
+        if mask is not None:
+            a *= K.cast(mask, K.floatx())
+
+        a /= K.cast(K.sum(a, axis=1, keepdims=True) + K.epsilon(), K.floatx())
+
+        weighted_input = x * K.expand_dims(a)
+
+        result = K.sum(weighted_input, axis=1)
+
+        if self.return_attention:
+            return [result, a]
+        
+        return result
+
 
