ad01d432f1a46787b9985d75f157c2c603a2e81e
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-31 00:31:18 +0100

    -mcreate build_attention_RNN

--- sentiment_analysis/models/nn_models.py
+++ sentiment_analysis/models/nn_models.py
@@ -44,3 +44,58 @@ def get_RNN(unit=LSTM, cells=64, bi=False, return_sequences=True, dropout_U=0.,
         return Bidirectional(rnn)
     else:
         return rnn
+
+
+def build_attention_RNN(embeddings, classes, max_length, unit=LSTM, cells=64,
+                        layers=1, **kwargs):
+    # parameters
+    bi = kwargs.get("bidirectional", False)
+    noise = kwargs.get("noise", 0.)
+    dropout_words = kwargs.get("dropout_words", 0)
+    dropout_rnn = kwargs.get("dropout_rnn", 0)
+    dropout_rnn_U = kwargs.get("dropout_rnn_U", 0)
+    dropout_attention = kwargs.get("dropout_attention", 0)
+    dropout_final = kwargs.get("dropout_final", 0)
+    attention = kwargs.get("attention", None)
+    final_layer = kwargs.get("final_layer", False)
+    clipnorm = kwargs.get("clipnorm", 1)
+    loss_l2 = kwargs.get("loss_l2", 0.)
+    lr = kwargs.get("lr", 0.001)
+
+    model = Sequential()
+    model.add(embeddings_layer(max_length=max_length, embeddings=embeddings, trainable=False,
+                               masking=True, scale=False, normalize=False))
+
+    if noise > 0:
+        model.add(GaussianNoise(noise))
+    if dropout_words > 0:
+        model.add(Dropout(dropout_words))
+
+    for i in range(layers):
+        rs = (layers > 1 and i < layers - 1) or attention
+        model.add(get_RNN(unit, cells, bi, return_sequences=rs, dropout_U=dropout_rnn_U))
+
+        if dropout_rnn > 0:
+            model.add(Dropout(dropout_rnn))
+    
+    if attention == "memory":
+        model.add(AttentionWithContext())
+        if dropout_attention > 0:
+            model.add(Dropout(dropout_attention))
+    if attention == "simple":
+        model.add(Attention())
+        if dropout_attention > 0:
+            model.add(Dropout(dropout_attention))
+    
+    if final_layer:
+        model.add(MaxoutDense(100, W_constraint=maxnorm(2)))
+        if dropout_final > 0:
+            model.add(Dropout(dropout_final))
+
+    model.add(Dense(classes, activity_regularizer=l2(loss_l2)))
+    model.add(Activation("softmax"))
+
+    model.compile(optimizer=Adam(clipnorm=clipnorm, lr=lr),
+                 loss="categorical_crossentropy")
+
+    return model
