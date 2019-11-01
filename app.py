defbf1a1b1a5f03f996f4ae1e4d094946213a8db
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-11-01 21:48:53 +0100

    -mcreate html and css for text generation

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ app = Flask(__name__)
 def index():
   return render_template("index.html")
 
-
 @app.route("/Sentiment", methods=['GET', 'POST'])
 def sentiment():
   return render_template("sentiment.html")
+
+@app.route("/Generation", methods=['GET', 'POST'])
+def text_generation():
+  return render_template("text_generation.html")
 
 if __name__ == "__main__":
   app.run(debug=True, host="localhost", port=5004)
