5aa4a3a3899ec9a75184dbe9781b33399bab431d
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-11-01 22:49:04 +0100

    -mcreate Attention class with init and build methods

---
+++ text_generation/Attention.py
@@ -0,0 +1,22 @@
+from keras.engine import InputSpec, Layer
+from keras import backend as K
+from keras import initializers
+
+
+class Attention(Layer):
+    def __init__(self, return_attention=True, **kwargs):
+        self.init = initializers.get('uniform')
+        self.supports_masking = True
+        self.return_attention = return_attention
+        super().__init__(**kwargs)
+    
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(ndim=3)]
+        assert len(input_shape) == 3
+
+        self.W = self.add_weight(shape=(input_shape[-1], 1),
+                                 name="{}_W".format(self.name),
+                                 initializer=self.init)
+        self.trainable_weights = [self.W]
+        super().build(input_shape)
+    
