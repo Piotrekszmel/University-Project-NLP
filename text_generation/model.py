30c5096b553beaadc2b2a2fa235dcaa2dbee22c6
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-11-05 16:50:33 +0100

    -mcreate text_generation_model function

--- text_generation/model.py
+++ text_generation/model.py
@@ -5,3 +5,17 @@ from keras.models import Model
 from keras import backend as K
 from Attention import Attention
 
+
+def text_generation_model(num_classes, cfg, context_size, weights_path,
+                          dropout=0.0, optimizer=RMSprop(lr=4e-3, rho=0.99)):
+  '''
+  Builds the model architecture for text generation and
+  loads the specified weights for the model.
+  '''
+
+  input = Input(cfg["max_length", ], name="input")
+  embedded = Embedding(num_classes, cfg["dim_embeddings"], input_length=cfg["max_length"],
+                      name="embedding")(input)
+  
+  if dropout > 0.0:
+    embedded = SpatialDropout1D(dropout, name="dropout")(embedded)
