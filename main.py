e3d03a076d581a430a190fc0e0cdb065d1a9d2b4
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-11-13 23:14:23 +0100

    create weights for 4 LSTM model with 128 rnn_size

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from sentiment_analysis.models.sentiment_model import Sentiment_Analysis
 
-Sentiment_Analysis("datastories.twitter", 300, 50, True, False)
+import os
+from sentiment_analysis.evaluate import predict_class
+
+tweets, predicted_y, label = predict_class(["I am happy", "I am sad :(", "Poland is a country"], [2,0,1], "datastories.twitter", 300)
+os.system("clear")
+for predict_y, label in zip(predicted_y, label):
+    print(float(predict_y), ' | ', label)
